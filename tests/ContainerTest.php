43d744b1926e68b7ff3b4ee1c59a0cd0363a3709
David Lombera <david.lomberlan@gmail.com>
2023-07-10 17:20:06 -0600

    test: add tests for basic entry resolution

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -5,11 +5,31 @@ declare(strict_types=1);
 namespace Phetit\Container\Tests;
 
 use Phetit\Container\Container;
+use Phetit\Container\Exception\EntryNotFoundException;
 use PHPUnit\Framework\TestCase;
 
 class ContainerTest extends TestCase
 {
-    public function testContainerResolvesClosureEntry(): void
+    public function testHasReturnsFalseOnMissingEntry(): void
+    {
+        $container = new Container();
+
+        $hasEntry = $container->has('foo');
+
+        self::assertFalse($hasEntry);
+    }
+
+    public function testHasReturnsTrueOnRegisteredEntry(): void
+    {
+        $container = new Container();
+
+        $container->register('foo', fn() => 'bar');
+        $hasEntry = $container->has('foo');
+
+        self::assertTrue($hasEntry);
+    }
+
+    public function testResolvesClosureEntry(): void
     {
         $container = new Container();
 
@@ -18,5 +38,15 @@ class ContainerTest extends TestCase
         $resolve = $container->get('foo');
 
         self::assertEquals('bar', $resolve);
+    }
+
+    public function testThrowsExceptionWhenTryingToResolveMissingEntry(): void
+    {
+
+        $container = new Container();
+
+        self::expectExceptionObject(new EntryNotFoundException());
+
+        $container->get('foo');
     }
 }
